731e6f5f84c1b973f8a2181124c906ec7ebbafcc
Cory Chatman <cc4chat@gmail.com>
2022-12-06 20:03:33 -0500

    navbar

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,18 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import {
-    Link,
-} from 'react-router-dom';
-
-export default function Navbar (){ 
-    return <>
-    <div>
-        HOME
-        <nav>
-            <Link to='./'>Home Page</Link> &nbsp;
-            <Link to='./about'>About Page</Link>
-        </nav>
-    </div>
-    
-    </>;
-
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <div><Nav.Link href="App.js">Home</Nav.Link></div>
+           <div> <Nav.Link href="portfolio.jsx">Portfolio</Nav.Link></div>
+           <div><Nav.Link href="about.jsx">About</Nav.Link></div>
+           <div><Nav.Link href="contact.jsx">Contact Me</Nav.Link></div>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Portfoilo
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Contact Me</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default NavBar;
---
+++ src/components/portfolio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function portfolio() {
+  return <>
+    <div>
+        <h2>CORY'S PORTFOLIO</h2>
+    </div>
+  </>
+}
