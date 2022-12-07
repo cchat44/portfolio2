675810d1ae36c72010be552d1e30efe5e9053c5e
Cory Chatman <cc4chat@gmail.com>
2022-12-06 22:45:31 -0500

    updated navbar

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 export default function About() {
-  return <>
+  return ( <>
     <div classname='pb-5'>
        <h2> ABOUT ME </h2>
         </div>
@@ -12,4 +12,5 @@ Software Engineer
 Atlanta, GA | 678-789-9582 | chatmancoryr@gmail.com | linkedin.com/in/cory-chatman-0ba885252/ 
 </div>
   </>
+  )
 }
--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,32 +1,18 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavBar() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <div><Nav.Link href="App.js">Home</Nav.Link></div>
-           <div> <Nav.Link href="portfolio.jsx">Portfolio</Nav.Link></div>
-           <div><Nav.Link href="about.jsx">About</Nav.Link></div>
-           <div><Nav.Link href="contact.jsx">Contact Me</Nav.Link></div>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Portfoilo
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Contact Me</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default navbar() {
+  return <>
+    <div className='navbar'>navbar</div>
+
+    <div className='links'>
+      <Link to ='/'> Home </Link>
+      <Link to ='/portfolio'> Portfolio </Link>
+      <Link to ='/about'> About </Link>
+      <Link to ='/contact'> Contact Me</Link>
+    </div>
+    </>
+  
 }
-
-export default NavBar;
